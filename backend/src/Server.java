12d65d65546f5a5bf2881cec1f67f9d1834832b5
Zulalm <zulalmolla@gmail.com>
2023-11-30 17:02:59 +0300

    implemented a sample code to send requests

--- backend/src/Server.java
+++ backend/src/Server.java
@@ -1,21 +1,19 @@
 import java.io.*;
 import java.net.*;
-class TCPServer {
+
+class Server {
     public static void main(String argv[]) throws Exception
     {
         String clientSentence;
         String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(6789);
+        ServerSocket welcomeSocket = new ServerSocket(8080);
 
-        while(true) {
+        while (true) {
             Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-                    new BufferedReader(new
-                            InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient =
-                    new DataOutputStream(connectionSocket.getOutputStream());
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             clientSentence = inFromClient.readLine();
-            capitalizedSentence = clientSentence + '\n';
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
             outToClient.writeBytes(capitalizedSentence);
         }
     }
