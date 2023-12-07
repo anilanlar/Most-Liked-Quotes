d8c71c53cfab23bdaed84f939789ac17206a0031
Zulalm <zulalmolla@gmail.com>
2023-12-08 00:19:25 +0300

    get_quotes request is implemented

--- backend/src/Server.java
+++ backend/src/Server.java
@@ -3,16 +3,68 @@ import java.net.*;
 import java.util.Scanner;
 
 class Server {
+    private static int numOfUsers = 0;
+    private static String quotesFileName = "quotes.txt";
+    private static String usersFileName = "users.txt";
+
+    private static boolean checkUserExists(String username) {
+        try {
+            Scanner in = new Scanner(new File(usersFileName));
+            while (in.hasNext()) {
+                String line = in.nextLine();
+                String[] params = line.split(" ");
+                if (params[1].compareTo(username) == 0) {
+                    in.close();
+                    return true;
+                }
+            }
+            in.close();
+            return false;
+        } catch (IOException e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    private static void registerUser(String username, String password) throws Exception {
+        try {
+            if (checkUserExists(username)) {
+                throw new Exception("Username is already given");
+            } else {
+                numOfUsers++;
+                FileWriter fileWriter = new FileWriter(usersFileName, true);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(numOfUsers + " " + username + " " + password);
+                bufferedWriter.newLine();
+                bufferedWriter.close();
+                fileWriter.close();
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static String getQuotes() {
+        String quotes = "";
+        try {
+            Scanner in = new Scanner(new File(quotesFileName));
+            while (in.hasNext()) {
+                String line = in.nextLine();
+                quotes = quotes + line + "\n";
+            }
+            in.close();
+
+        } catch (IOException e) {
+            System.out.println(e);
+
+        }
+        return quotes;
+    }
     public static void main(String argv[]) throws Exception
     {
-        String fileName = "database.txt";
-        InputStream in = new FileInputStream(fileName); // file handling system will be decided
-        // Scanner in = new Scanner(new File(fileName));
-
         String requestType;
-        String capitalizedSentence;
         ServerSocket welcomeSocket = new ServerSocket(8080);
-
 
         while (true) {
             Socket connectionSocket = welcomeSocket.accept();
@@ -20,10 +72,12 @@ class Server {
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             // read type of the request
             requestType = inFromClient.readLine(); // available types are "get_quotes", "upvote_quote" ,
-                                                   // "downvote_quote";
+                                                   // "downvote_quote", "signup", "login";
             // client request is read
             if (requestType.compareTo("get_quotes") == 0) {
                 // return data to client
+                String quotes = Server.getQuotes();
+                outToClient.writeBytes(quotes);
             } else if (requestType.compareTo("upvote_quote") == 0) {
                 // get more data from client
                 // process data
@@ -33,9 +87,17 @@ class Server {
                 // process data
                 // return info message
             }
+            else if (requestType.compareTo("sign_up") == 0) {
+                // get more data from client
+                // process data
+                // return info message
+            } else if (requestType.compareTo("log_in") == 0) {
+                // get more data from client
+                // process data
+                // return info message
+            }
+            outToClient.close();
 
-            capitalizedSentence = "info message to client";
-            outToClient.writeBytes(capitalizedSentence);
         }
     }
 }
