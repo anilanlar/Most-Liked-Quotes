59b9e1863a3dc14843f130a6e1da05053d463050
Zulalm <zulalmolla@gmail.com>
2023-12-07 13:20:22 +0300

    request types are added

--- backend/src/Server.java
+++ backend/src/Server.java
@@ -1,19 +1,40 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 class Server {
     public static void main(String argv[]) throws Exception
     {
-        String clientSentence;
+        String fileName = "database.txt";
+        InputStream in = new FileInputStream(fileName); // file handling system will be decided
+        // Scanner in = new Scanner(new File(fileName));
+
+        String requestType;
         String capitalizedSentence;
         ServerSocket welcomeSocket = new ServerSocket(8080);
+
 
         while (true) {
             Socket connectionSocket = welcomeSocket.accept();
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            // read type of the request
+            requestType = inFromClient.readLine(); // available types are "get_quotes", "upvote_quote" ,
+                                                   // "downvote_quote";
+            // client request is read
+            if (requestType.compareTo("get_quotes") == 0) {
+                // return data to client
+            } else if (requestType.compareTo("upvote_quote") == 0) {
+                // get more data from client
+                // process data
+                // return info message
+            } else if (requestType.compareTo("downvote_quote") == 0) {
+                // get more data from client
+                // process data
+                // return info message
+            }
+
+            capitalizedSentence = "info message to client";
             outToClient.writeBytes(capitalizedSentence);
         }
     }
